b2ab726069d7147a11be993e6f966ddebd0e8521
Natasha Matthews <natashamatthews90@gmail.com>
2022-01-13 19:36:11 -0600

    made menu and menuitem class

---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,57 @@
+package restaurant;
+
+public class MenuItem {
+    private String name;
+    private Double price;
+    private String description;
+    private String category;
+    private Boolean isNew;
+
+    public MenuItem(String name, Double price, String description, String category, Boolean isNew) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Boolean getNew() {
+        return isNew;
+    }
+
+    public void setNew(Boolean aNew) {
+        isNew = aNew;
+    }
+}
